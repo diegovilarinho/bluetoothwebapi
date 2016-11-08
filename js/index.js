b4bb095f10c8b7a0adc67d50ce8c58d34fd985da
Diego Vilarinho <diegovilarinhoo@gmail.com>
2016-11-08 01:31:04 -0200

    Ajustes de javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 'use strict';
 
-import scanDevices from './modules/scanDevices';
+const scanDevices = () => {
+	navigation.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+	.then(device => {})
+	.catch(error => { console.log(error); });
+};
+
 scanDevices();
