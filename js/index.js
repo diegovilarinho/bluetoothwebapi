22d05e1894b46e8621f7931c8d9569e98f01591c
Diego Vilarinho <diegovilarinhoo@gmail.com>
2016-11-08 01:33:43 -0200

    Ajustes de javascript

--- js/index.js
+++ js/index.js
@@ -1,9 +1,57 @@
-'use strict';
+class GenericDevice {
 
-const scanDevices = () => {
-	navigation.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-	.then(device => {})
-	.catch(error => { console.log(error); });
-};
+  constructor() {
+    this.device = null;
+    this.onDisconnected = this.onDisconnected.bind(this);
+  }
 
-scanDevices();
+  request() {
+    let options = {
+      "filters": [{
+        "name": "foo"
+      }],
+      "optionalServices": ["device_information"]
+    };
+    return navigator.bluetooth.requestDevice(options)
+    .then(device => {
+      this.device = device;
+      this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+      return device;
+    });
+  }
+
+  connect() {
+    if (!this.device) {
+      return Promise.reject('Device is not connected.');
+    } else {
+      return this.device.gatt.connect();
+    }
+  }
+  
+  readManufacturername() {
+    return this.device.gatt.getPrimaryService("device_information")
+    .then(service => service.getCharacteristic("manufacturer_name_string"))
+    .then(characteristic => characteristic.readValue());
+  }
+
+  disconnect() {
+    if (!this.device) {
+      return Promise.reject('Device is not connected.');
+    } else {
+      return this.device.gatt.disconnect();
+    }
+  }
+
+  onDisconnected() {
+    console.log('Device is disconnected.');
+  }
+}
+
+var genericDevice = new GenericDevice();
+
+document.querySelector('button').addEventListener('click', function() {
+  genericDevice.request()
+  .then(_ => genericDevice.connect())
+  .then(_ => { /* Do something with genericDevice */})
+  .catch(error => { console.log(error) });
+});
